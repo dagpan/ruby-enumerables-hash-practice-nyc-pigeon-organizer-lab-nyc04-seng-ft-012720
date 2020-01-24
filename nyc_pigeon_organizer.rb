191479117e7b23356a821f4a58ea4b3b3cfec689
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-24 19:02:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -38,67 +38,28 @@ def nyc_pigeon_organizer(data)
       i += 1
   end
   pp name_hash
-  result_hash = name_hash.collect
   i = 0
-  while i < 
-  if array_purple.include?(name_array[i]) 
-     name_hash[name_array[i]][:color] << "purple"
-  elsif array_grey.include?(name_array[i])
-     name_hash[name_array[i]][:color] << "grey"
-  elsif array_white.include?(name_array[i])
-     name_hash[name_array[i]][:color] << "white"
-  elsif array_brown.include?(name_array[i])
-     name_hash[name_array[i]][:color] << "brown"
-  elsif array_subway.include?(name_array[i])
-     name_hash[name_array[i]][:color] << "Subway"
-  elsif array_centralpark.include?(name_array[i])
-     name_hash[name_array[i]][:color] << "Central Park"
-  elsif array_library.include?(name_array[i])
-     name_hash[name_array[i]][:color] << "Library"
-  elsif array_cityhall.include?(name_array[i])
-     name_hash[name_array[i]][:color] << "City Hall"
+  while i < name_array.length do
+      if array_purple.include?(name_array[i]) 
+         name_hash[name_array[i]][:color] << "purple"
+      elsif array_grey.include?(name_array[i])
+         name_hash[name_array[i]][:color] << "grey"
+      elsif array_white.include?(name_array[i])
+         name_hash[name_array[i]][:color] << "white"
+      elsif array_brown.include?(name_array[i])
+         name_hash[name_array[i]][:color] << "brown"
+      elsif array_subway.include?(name_array[i])
+         name_hash[name_array[i]][:color] << "Subway"
+      elsif array_centralpark.include?(name_array[i])
+         name_hash[name_array[i]][:color] << "Central Park"
+      elsif array_library.include?(name_array[i])
+         name_hash[name_array[i]][:color] << "Library"
+      elsif array_cityhall.include?(name_array[i])
+         name_hash[name_array[i]][:color] << "City Hall"
+      end
   i += 1
   end
-  
-  while i < array_purple.length do
-      name_hash[array_purple[i]][:color] = ["purple"
-      i += 1
-  end
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  #array_purple.each_with_index do |name, index|
- #     name_hash[name_array[i]][:color] 
-  
-  
-  
-
-#      if array_purple.include?(name_array[i])
-  
-#  name_array.each_with_index do |name, index|
-#      if array_purple.include?(name_array[i])
-#         array_purple
-  
-  
-  
-  
-
-  
-#  data[:gender][:male].
-#   data.reduce({}) do |memo, (key, value)|
-#   memo
-     
-     
-     
-#       pp memo
-#       pp key
-#       pp value
+  pp name_hash
 end
 
 
