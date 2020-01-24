53a2f46abcc4ce5dc640f97f7af4dc038cad1a6c
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-24 18:52:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -25,9 +25,7 @@ def nyc_pigeon_organizer(data)
   array_library = data[:color]["Library"]
   array_cityhall = data[:color]["City Hall"]
   name_array = array1.concat(array2)
-  name_hash = Hash[name_array.collect {|name| [name, {}]}]
-  pp name_array
-  pp name_hash
+  name_hash = Hash[name_array.collect {|name| [name, {:color = [], :lives = []}]}]
   i = 0
   while i < 5 do
       name_hash[name_array[i]][:gender] = ["male"]
@@ -40,8 +38,22 @@ def nyc_pigeon_organizer(data)
       i += 1
   end
   pp name_hash
+  result_hash = name_hash.collect
+  i = 0
+  if array_purple.include?(name_array[i]) do
+     name_hash[name_array[i]][:color] = ["purple"]
  
- 
+  while i < array_purple.length do
+      name_hash[array_purple[i]][:color] = ["purple"]
+      i += 1
+  end
+  
+  
+  
+  
+  
+  
+  
   
   
   #array_purple.each_with_index do |name, index|
@@ -49,7 +61,7 @@ def nyc_pigeon_organizer(data)
   
   
   
-#     if array_purple.include?(name_array[i])  
+
 #      if array_purple.include?(name_array[i])
   
 #  name_array.each_with_index do |name, index|
