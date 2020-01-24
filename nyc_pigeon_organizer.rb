c4f91fac06dd799cf9579a81af2e668e13c75ed8
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-24 20:04:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -45,7 +45,7 @@ def nyc_pigeon_organizer(data)
   pp array1
   pp array2
   m = 0
-  array2.each |name| do
+  array2.each |name|
      while m < array1.length
          if array1[m].include?(name)
             array1.delete_at(m)
